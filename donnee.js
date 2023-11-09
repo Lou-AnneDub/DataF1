8f9c14cdf79115651165163324c92e895f38bb4d
Lou-AnneDub <dubillelouanne@gmail.com>
2023-11-09 14:40:45 +0100

    supression import gsap

--- donnee.js
+++ donnee.js
@@ -1,17 +1,27 @@
 // récupération de données et création du graphique
-d3.json("pilotes.json").then(function(data){
+d3.json("pilotes.json",function(data){
 
-    d3.select("#barres")
-        .selectAll(".histobarre")
+    var selectedYear = "2000";
+
+    var hisogram=d3.hisogram()
+        .value(function(d){
+            return d[2];
+        })
+        .thresholds(x.ticks(3));
+    
+    var barres = hisogram(data);
+
+    /*d3.select("#barres")
+        .selectAll(".histo")
         .data(data)
         .join("g")
-        .attr("class",'histobarre')
+        .attr("class",'histo')
         .attr("transform", (d,i)=>`translate(${i*180},0)`);
     
-    d3.select(".histobarre")
+    d3.select(".histo")
         .append("rect")
         .attr("width", 180)
-        .attr("height", d=> d.Points)
-        .style("fill","blue");
+        .attr("height", d=> d.points)
+        .style("fill","blue");*/
 
 });
