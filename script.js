5da09c03e5a4aa4a3bf751b2d809424e16d76210
Lou-AnneDub <dubillelouanne@gmail.com>
2023-11-09 13:04:33 +0100

    :construction:

--- script.js
+++ script.js
@@ -21,20 +21,4 @@ const maTimeline = gsap.timeline({
     .to('.f44',{})
     .to('.f55',{})
 
-// récupération de données et création du graphique
-d3.json("pilotes.json", function(data){
 
-    d3.select("#barres")
-        .selectAll(".histobarre")
-        .data(data)
-        .join("g")
-        .attr("class",'histobarre')
-        .attr("transform", (d,i)=>`translate(${i*180},0)`);
-    
-    d3.select(".histobarre")
-        .append("rect")
-        .attr("width", 180)
-        .attr("height", d=> d.Points)
-        .style("fill","blue");
-
-});
