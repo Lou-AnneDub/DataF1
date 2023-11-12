b62f68524082b7daa2c95bd463bb203a12bd689b
Lou-AnneDub <dubillelouanne@gmail.com>
2023-11-12 18:13:26 +0100

    :poop:

--- donnee.js
+++ donnee.js
@@ -1,7 +1,47 @@
 // récupération de données et création du graphique
-d3.json("pilotes.json",function(data){
+d3.json("pilotes.json").then(function(data){
+    
+    // Sélectionnez le conteneur
+    var svg = d3.select(".graphiques")
+                    .append("svg")
+                    .attr("width", 600)
+                    .attr("height", 475);
 
-    var selectedYear = "2000";
+
+    function createHisto(yearData){
+        console.log("YearData :", yearData);
+
+        svg.selectAll(".bar").remove();
+
+        svg.selectAll(".bar")
+                    .data(yearData)
+                    .join("rect")
+                    .attr("class", "bar")
+                    .attr("width", 180)
+                    .attr("height",  d=> d.points)
+                    .attr("transform", (d,i)=>`translate(${i*180},0)`);;
+
+    }
+
+    //Fonction pour le changement d'année
+    function changeYear(offset) {
+        var selectedYearElement = document.getElementById("selectedYear");
+        var currentYear = parseInt(selectedYearElement.textContent);
+        var newYear = currentYear + offset;
+
+        //vérifie si l'année est bien dans le tableau
+        if (data[newYear]) {
+            selectedYearElement.textContent = newYear;
+            createHisto(data[newYear]);
+        }
+    }
+
+    //Initie l'histogramme à l'année 2000
+    createHisto(data["2000"]);
+
+}).catch(function (error) {
+    console.error("Erreur lors du chargement des données :", error);
+});
 
     /*test
     var hisogram=d3.hisogram()
@@ -11,12 +51,12 @@ d3.json("pilotes.json",function(data){
         .thresholds(x.ticks(3));*/
     
 
-    d3.select("#barres")
+    /*d3.select("#barres")
         .data(data)
         .join("rect")
         .attr("width", 180)
         .attr("height", d=> d.points)
-        .style("fill","blue");
+        .style("fill","blue");*/
 
     /*d3.select("#barres")
         .selectAll(".histo")
@@ -31,4 +71,4 @@ d3.json("pilotes.json",function(data){
         .attr("height", d=> d.points)
         .style("fill","blue");*/
 
-});
+//});
