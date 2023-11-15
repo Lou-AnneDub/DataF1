0f61977d1ce1d052a24ffc0cfcc3437c54582b08
Lou-AnneDub <dubillelouanne@gmail.com>
2023-11-15 14:55:16 +0100

    :coffin:

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// const maTimeline = gsap.timeline({
-//     defaults: {
-//         opacity:0,
-//         duration:1
-//     }
-// })
-//     .to('.f11',{})
-//     .to('.f22',{})
-//     .to('.f33',{})
-//     .to('.f44',{})
-//     .to('.f55',{})
-
 document.addEventListener('DOMContentLoaded', function() {
     const maTimeline = gsap.timeline({
         defaults: {
